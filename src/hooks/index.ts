bba72b03674abc0bf211419e83886be445bebedf
Eisha313 <Eisha313@users.noreply.github.com>
2024-12-25 22:04:12 +0000

    refactor: extract custom hooks and optimize component performance

---
+++ src/hooks/index.ts
@@ -0,0 +1,8 @@
+export { useProducts } from './useProducts';
+export { useCart } from './useCart';
+export { useFilters } from './useFilters';
+export { useLocalStorage } from './useLocalStorage';
+export { useDebounce, useDebouncedCallback } from './useDebounce';
+
+// Re-export types for convenience
+export type { Product, CartItem, FilterState, PriceRange } from '../types/product';
